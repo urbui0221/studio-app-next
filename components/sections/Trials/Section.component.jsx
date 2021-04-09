d19da088d6fa3ae251183d81e2dd6c51c7b6a9f4
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-04-09 13:58:03 +0530

    SVG FIX

--- components/sections/Trials/Section.component.jsx
+++ components/sections/Trials/Section.component.jsx
@@ -1,81 +1,106 @@
-import React from 'react'
-import styled, { css } from 'styled-components'
-import Button from '../../ui/Button.component'
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Button from '../../ui/Button.component';
 
-const Section = ({Icon,heading,description,btnTxt,children,...otherProps}) => {
-    return (
-       <>
-{   otherProps.bgColor && <svg style={{marginBottom: -10}} version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-	 viewBox="0 0 1920 150">
-<path class="st0" fill={'#f5f5f5'} d="M1920,150V0c-285.7,8.1-475.1,27.3-598.6,44.3c-58.7,8.1-147.1,21.9-268,17.7c-54.5-1.9-117.3-8.4-243-21.5
-	c-122.8-12.8-125.1-15-176-18.9c-37.1-2.9-141.3-9.8-272.3-2C267,25.3,143,39.6,0,75v75H1920z"/>
-</svg>
-    
-}        <CommonSection 
-       {...otherProps}>
-          <div className="content-container">
-                { Icon && <div className="content-container--icon">
-                            <Icon /> 
-                        </div>}
-                <h1>{heading}</h1>
-                { description && <p>{description}</p> }
-                    {btnTxt && <TrBtn>{btnTxt}</TrBtn>}
-                { children }
-          </div>
-       </CommonSection>
-{ otherProps.bgColor && <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-	 viewBox="0 0 1920 150">
-<path class="st0" fill={'#f5f5f5'} d="M1920,0v150c-173.5-11.6-314.8-28-413.4-41.2c-190.3-25.4-208.1-36.9-301.3-36.7
-	c-177.1,0.4-194.2,42-391.5,42.3c-104.4,0.1-203.9-11.4-274.3-19.5C408.1,79.7,388.6,69,319.2,65.8C243.6,62.3,134.9,68.9,0,117.7V0
-	H1920z"/>
-</svg>
-}       </>
-    )
-}
+const Section = ({
+	Icon,
+	heading,
+	description,
+	btnTxt,
+	children,
+	...otherProps
+}) => {
+	return (
+		<>
+			{otherProps.bgColor && (
+				<svg
+					style={{ marginBottom: -10, fill: '#f5f5f5' }}
+					version='1.1'
+					xmlns='http://www.w3.org/2000/svg'
+					x='0px'
+					y='0px'
+					viewBox='0 0 1920 150'
+				>
+					<path
+						d='M0.3,150.8h1920v-65c-92.7-18.9-237.5-42.3-415.8-44.3c-249.7-2.8-279.5,39.6-526.6,43.5
+                c-316.5,4.9-333.1-63.7-627-39.2C231.6,55.7,0.3,86.6,0.3,86.6c0,0,0,0,0,0V150.8z'
+					/>
+				</svg>
+			)}{' '}
+			<CommonSection {...otherProps}>
+				<div className='content-container'>
+					{Icon && (
+						<div className='content-container--icon'>
+							<Icon />
+						</div>
+					)}
+					<h1>{heading}</h1>
+					{description && <p>{description}</p>}
+					{btnTxt && <TrBtn>{btnTxt}</TrBtn>}
+					{children}
+				</div>
+			</CommonSection>
+			{otherProps.bgColor && (
+				<svg
+					style={{ fill: '#f5f5f5', marginTop: -2 }}
+					version='1.1'
+					xmlns='http://www.w3.org/2000/svg'
+					x='0px'
+					y='0px'
+					viewBox='0 0 1920 150'
+				>
+					<path
+						xmlns='http://www.w3.org/2000/svg'
+						d='M0.8,2.1h1920v65c-92.7,18.9-237.5,42.3-415.8,44.3c-249.7,2.8-279.5-39.6-526.6-43.5  c-316.5-4.9-333.1,63.7-627,39.2C232,97.2,0.8,66.3,0.8,66.3c0,0,0,0,0,0V2.1z'
+					/>
+				</svg>
+			)}{' '}
+		</>
+	);
+};
 
-export default Section
+export default Section;
 
 const CommonSection = styled.section`
-margin: ${props => props.margin || '0rem'};
-text-align: center;
-font-family : var(--font3);
-width: 100%;
-background-color : ${props => props.bgColor || "#fff"};
-display :flex;
-flex-direction: column;
-justify-content:center;
-align-items: center;
-padding : 0rem 2rem;
-h1{
-    font-size : 4.5rem;
-    font-weight : 800;
-}
-p{
-    margin-top : 1.5rem;
-    max-width: 80rem;
-    margin: 0 auto;
-}
-@media only screen and (max-width : 775px){
-   h1{
-    font-size : 3rem;
-   }
-   p{
-       max-width : 100%;
-   }
-}
-.content-container{
-    position: relative;
-    top : ${props => props.moveTop};
-    &--icon{
-        margin-bottom : 1.5rem;
-    }
-}
-
-`
+	margin: ${(props) => props.margin || '0rem'};
+	text-align: center;
+	font-family: var(--font3);
+	width: 100%;
+	background-color: ${(props) => props.bgColor || '#fff'};
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 0rem 2rem;
+	h1 {
+		font-size: 4.5rem;
+		font-weight: 800;
+	}
+	p {
+		margin-top: 1.5rem;
+		max-width: 80rem;
+		margin: 0 auto;
+	}
+	@media only screen and (max-width: 775px) {
+		h1 {
+			font-size: 3rem;
+		}
+		p {
+			max-width: 100%;
+		}
+	}
+	.content-container {
+		position: relative;
+		top: ${(props) => props.moveTop};
+		&--icon {
+			margin-bottom: 1.5rem;
+		}
+	}
+`;
 
 const TrBtn = styled(Button)`
-padding : 1.5rem 3rem;
-background-color : var(--primary);
-text-transform : uppercase;
-margin-top : 3rem;
-`
+	padding: 1.5rem 3rem;
+	background-color: var(--primary);
+	text-transform: uppercase;
+	margin-top: 3rem;
+`;
